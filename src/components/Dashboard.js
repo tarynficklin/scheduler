968323d7646758a805bcd8e2f17f4a7097a334ff
TarynFicklin <TarynFicklin@gmail.com>
2018-08-02 17:17:46 -0600

    Displaying Data on Frontend

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -12,6 +12,9 @@ class Dashboard extends Component {
 	}
 
 	componentDidMount() {axios.get('/api/trips').then(results => this.setState({tripCards: results.data}))}
+	componentDidUpdate() {axios.get('/api/trips').then(results => this.setState({tripCards: results.data}))}
+
+	deleteTrip(id) {axios.delete(`api/trips/${id}`)}
 
 	render () {
 		const {tripCards} = this.state;
@@ -22,6 +25,14 @@ class Dashboard extends Component {
 					return (
 						<TripCard
 							key={i}
+							id={e.trip_id}
+							location={e.trip_location}
+							startDate={e.trip_start_date}
+							endDate={e.trip_end_date}
+							budget={e.trip_budget}
+							packingList={e.trip_packing_list}
+							schedule={e.trip_schedule}
+							deleteTrip={this.deleteTrip}
 						/>
 					)
 				})
--- src/components/Dashboard/TripCard.js
+++ src/components/Dashboard/TripCard.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './TripCard.css';
 
 export default function TripCard(props) {
+	const {id, location, startDate, endDate, budget, packingList, schedule, deleteTrip} = props;
 	return (
 		<div className="trip-card">
-			<button>X</button>
-			<p>id:</p>
-			<p>title:</p>
-			<p>date:</p>
-			<p>user id:</p>
+			<button onClick={() => deleteTrip(id)}>X</button>
+			<p>id: {id}</p>
+			<p>location: {location}</p>
+			<p>date: {startDate}-{endDate}</p>
+			<p>budget: {budget}</p>
+			<p>packingList: {JSON.stringify(packingList)}</p>
+			<p>schedule: {JSON.stringify(schedule)}</p>
 		</div>
 	)
 }
