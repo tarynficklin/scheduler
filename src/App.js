83860df22062b36b28339151c7afb723fb2ff8ba
TarynFicklin <38847691+TarynFicklin@users.noreply.github.com>
2018-08-07 02:21:16 -0600

    App.js cleanup

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
+//dependencies
 import React, { Component } from 'react';
 import {HashRouter, Switch, Route} from 'react-router-dom';
+import {updateUserData} from './ducks/auth0';
+import {connect} from 'react-redux';
 import axios from 'axios';
 import './App.css'
 
-import {connect} from 'react-redux';
-import {updateUserData} from './ducks/auth0';
-
-import Landing from './components/Landing';
+//components
+import Landing   from './components/Common/Landing';
+import Missing   from './components/Common/Missing';
 import Dashboard from './components/Dashboard';
-import Creator from './components/Creator';
+import Creator   from './components/Creator';
 import Scheduler from './components/Scheduler';
-import Missing from './components/Missing';
 
 class App extends Component {
+
   componentDidMount () {axios.get('/api/user-data').then(res => {this.props.updateUserData(res.data);})}
+
   render() {
     let {user} = this.props;
+    
     return (
       <div className="app">
         {
 					user.user_name ? (
           <HashRouter>
             <Switch>
-              <Route exact path="/" component={Dashboard}/>
-              <Route path="/new" component={Creator} />
+              <Route exact path="/"   component={Dashboard}/>
+              <Route path="/new"      component={Creator} />
               <Route path="/trip/:id" component={Scheduler} />
-              <Route path="/404" component={Missing} />
+              <Route path="/404"      component={Missing} />
             </Switch>
           </HashRouter>
 					) : <Landing />
