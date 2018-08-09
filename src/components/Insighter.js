d71525c00097e6f4030942fce6477016b3714523
TarynFicklin <38847691+TarynFicklin@users.noreply.github.com>
2018-08-09 16:17:33 -0600

    complete insighter functionality

--- src/components/Insighter.js
+++ src/components/Insighter.js
@@ -15,14 +15,22 @@ class Insighter extends Component {
 		this.state = {
 			trip_id: 0,
 			trip_location: "Trip",
-			trip_start_date: 0,
-			trip_end_date: 0,
+			trip_start_date: '1/1/20XX',
+			trip_end_date: '1/1/20XX',
 			trip_budget: 0,
 			trip_packing_list: [],
 			trip_schedule: []
 		}
-		this.getBudgetInput = this.getBudgetInput.bind(this);
-		this.updateBudget = this.updateBudget.bind(this);
+		//input bindings
+		this.getBudgetInput    = this.getBudgetInput.bind(this);
+		this.getLocationInput  = this.getLocationInput.bind(this);
+		this.getStartDateInput = this.getStartDateInput.bind(this);
+		this.getEndDateInput   = this.getEndDateInput.bind(this);
+		//axios bindings
+		this.updateBudget    = this.updateBudget.bind(this);
+		this.updateLocation  = this.updateLocation.bind(this);
+		this.updateStartDate = this.updateStartDate.bind(this);
+		this.updatEndDate    = this.updatEndDate.bind(this);
 	}
 
 	componentDidMount() {
@@ -40,13 +48,21 @@ class Insighter extends Component {
 	}
 
 	deleteTrip (id) {axios.delete(`api/trips/${id}`)}
-	getBudgetInput (val) {this.setState({trip_budget: val})}
-	updateBudget (id) {axios.put(`/api/trips/budget/${id}`, {budget: this.state.trip_budget})}
+	//input functions
+	getBudgetInput    (val) {this.setState({trip_budget: val})}
+	getLocationInput  (val) {this.setState({trip_location: val})}
+	getStartDateInput (val) {this.setState({trip_start_date: val})}
+	getEndDateInput   (val) {this.setState({trip_end_date: val})}
+	//axios functions
+	updateBudget    (id) {axios.put(`/api/trips/budget/${id}`,    {budget: this.state.trip_budget})}
+	updateLocation  (id) {axios.put(`/api/trips/location/${id}`,  {location: this.state.trip_location})}
+	updateStartDate (id) {axios.put(`/api/trips/startDate/${id}`, {startDate: this.state.trip_start_date})}
+	updatEndDate    (id) {axios.put(`/api/trips/endDate/${id}`,   {endDate: this.state.trip_end_date})}
 
 	render () {
 		
 		const {trip_id,	trip_location, trip_start_date,	trip_end_date, trip_budget, trip_schedule, trip_packing_list} = this.state;
-		const {getBudgetInput, updateBudget} = this;
+		const {getBudgetInput, getLocationInput, getStartDateInput, getEndDateInput, updateBudget, updateLocation, updateStartDate, updatEndDate} = this;
 
 		console.log(this.state)
 		return (
@@ -64,6 +80,15 @@ class Insighter extends Component {
 					startDate={trip_start_date}
 					endDate={trip_end_date}
 					id={trip_id}
+
+					getLocationInput={getLocationInput}
+					getStartDateInput={getStartDateInput}
+					getEndDateInput={getEndDateInput}
+
+					updateLocation={updateLocation}
+					updateStartDate={updateStartDate}
+					updatEndDate={updatEndDate}
+
 					deleteTrip={this.deleteTrip} />
 			</div>
 		)
--- src/components/Insighter/SettingsWidget.js
+++ src/components/Insighter/SettingsWidget.js
@@ -3,15 +3,16 @@ import {Link} from 'react-router-dom'
 import './SettingsWidget.css'
 
 export default function SettingsWidget(props) {
-	const {location, startDate, endDate, id, deleteTrip} = props
+	const {location, startDate, endDate, id, deleteTrip} = props;
+	const {getLocationInput, getStartDateInput, getEndDateInput, updateLocation, updateStartDate, updatEndDate} = props;
 	return (
 		<div className="settings-widget">
 			<h3>Settings Widget</h3>
 			<p>Current Location: {location}</p>
-			<input placeholder="location" />
+			<input value={location} onChange={(e) => getLocationInput(e.target.value)} onBlur={() => updateLocation(id)}/>
 			<p>Current Date: {startDate} - {endDate}</p>
-			<input placeholder="start date" />
-			<input placeholder="end date" />
+			<input value={startDate} onChange={(e) => getStartDateInput(e.target.value)} onBlur={() => updateStartDate(id)} />
+			<input value={endDate} onChange={(e) => getEndDateInput(e.target.value)} onBlur={() => updatEndDate(id)} />
 			<Link to="/"><button onClick={() => deleteTrip(id)}>Delete Trip</button></Link>
 		</div>
 	)
